0f6ced654a10778e91722c6b404629555ec3a29b
Kaung Htet ZIn <kaunghtetzin@Kaungs-MacBook-Air.local>
2023-04-11 21:03:13 +0630

    Added first project

--- python/digitalswap.py
+++
@@ -1,2 +0,0 @@
-num = input();
-print('{}{}'.format(num[1],num[0]));
--- python/matchfunction.py
+++
@@ -1,9 +0,0 @@
-name = input();
-
-match name:
-    case "Harry" | "Hermonie" | "Ron":
-        print("Gryffindor");
-    case "Draco":
-        print("Slytherin");
-    case _:
-        print("Who?");
